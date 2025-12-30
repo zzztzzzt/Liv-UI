148b1c7221567c73aa97c3182f1c204c99e5981c
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2025-12-31 03:31:03 +0800

    [feat] add BtnMatcha01. add TailwindCSS

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,9 @@
 <script setup lang="ts">
-
+    import BtnMatcha01 from './components/BtnMatcha01.vue';
 </script>
 
 <template>
-
+    <BtnMatcha01 />
 </template>
 
 <style scoped>
---
+++ src/components/BtnMatcha01.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="flex flex-col items-center">
+        <div class="flex">
+            <div class="w-28 h-16"></div>
+            <div class="w-46 h-16 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#C5E4E8] to-[#7FFFE5] shadow-[-6px_-6px_0_5px_rgba(0,0,0,0.25)]"></div>
+        </div>
+        <div class="w-48 h-17 flex justify-center items-center rounded-xl font-prosto-one text-[#C5B8B8] text-xl shadow-[0_0_5px_5px_rgba(0,0,0,0.05)] cursor-pointer">Let's Go</div>
+        <div class="flex">
+            <div class="w-46 h-16 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#C5E4E8] to-[#7FFFE5] shadow-[6px_6px_0_5px_rgba(0,0,0,0.25)]"></div>
+            <div class="w-28 h-16"></div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
