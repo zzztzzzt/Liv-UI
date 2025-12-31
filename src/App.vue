f303574cad12b7c7bb63812b914b26c0e2c964fc
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2025-12-31 21:55:30 +0800

    [feat] add BtnMatcha02. add new font

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup lang="ts">
     import BtnMatcha01 from './components/BtnMatcha01.vue';
+    import BtnMatcha02 from './components/BtnMatcha02.vue';
 </script>
 
 <template>
-    <BtnMatcha01 />
+    <!--BtnMatcha01 /-->
+    <BtnMatcha02 />
 </template>
 
 <style scoped>
---
+++ src/components/BtnMatcha02.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="relative">
+        <div class="rotate-90 absolute -top-30 left-7 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        <div class="rotate-65 absolute -top-30 -left-8 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        <div class="rotate-25 absolute -top-21 -left-21 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        <div class="-rotate-20 absolute -top-6 -left-26 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        <div class="-rotate-70 absolute top-9 -left-19 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        <div class="-rotate-135 absolute top-16 -left-1 w-26 h-9 rounded-tr-full rounded-bl-full bg-gradient-to-r from-[#7FFFE5] to-[#C5E4E8]"></div>
+        
+        <div class="rotate-45 w-45 h-15 flex justify-center items-center text-lg text-[#C5B8B8] border border-[#DFDFDF] rounded-full bg-[#E6FEFF] font-prosto-one cursor-pointer">Let's Go</div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
