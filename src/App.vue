46b7e013128fd6e0438d76c1fe0271c47ccb04ef
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-01-11 01:15:15 +0800

    [feat] add InputMatcha01 component

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,14 @@
     import BtnMatcha01 from './components/BtnMatcha01.vue';
     import BtnMatcha02 from './components/BtnMatcha02.vue';
     import BtnMatcha03 from './components/BtnMatcha03.vue';
+    import InputMatcha01 from './components/InputMatcha01.vue';
 </script>
 
 <template>
     <!--BtnMatcha01 /-->
     <!--BtnMatcha02 /-->
-    <BtnMatcha03 />
+    <!--BtnMatcha03 /-->
+    <InputMatcha01 />
 </template>
 
 <style scoped>
---
+++ src/components/InputMatcha01.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="h-23 w-81 flex justify-center items-center bg-linear-to-r from-[#8AFFC3] via-[#666666] to-[#ffffff] rounded-full">
+        <div class="h-21 w-79 flex justify-center items-center rounded-full bg-white">
+            <div class="h-16 w-74 flex justify-center items-center bg-conic-90 from-[#B5FF7D]/30 via-[#A7DACB]/70 to-[#B8B8B8]/30 rounded-full">
+                <input type="text" class="h-13 w-71 px-6 text-lg text-gray-400 bg-white rounded-full focus:outline-none">
+            </div>
+        </div>
+    </div>
+</template>
