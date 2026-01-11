33fb79f7b47e31042958717464f5fba3759878a5
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-01-12 01:02:16 +0800

    [feat] add InputMatcha02 component

--- src/App.vue
+++ src/App.vue
@@ -3,13 +3,15 @@
     import BtnMatcha02 from './components/BtnMatcha02.vue';
     import BtnMatcha03 from './components/BtnMatcha03.vue';
     import InputMatcha01 from './components/InputMatcha01.vue';
+    import InputMatcha02 from './components/InputMatcha02.vue';
 </script>
 
 <template>
     <!--BtnMatcha01 /-->
     <!--BtnMatcha02 /-->
     <!--BtnMatcha03 /-->
-    <InputMatcha01 />
+    <!--InputMatcha01 /-->
+    <InputMatcha02 />
 </template>
 
 <style scoped>
---
+++ src/components/InputMatcha02.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="relative h-16 w-115 flex justify-center items-center rounded-full bg-gradient-to-r from-[#6AFDC2] to-[#3F9774]">
+        <div class="absolute -z-3 -top-47 left-28 h-43 w-43 rounded-full bg-gradient-to-r from-[#9BFFCF] to-[#56929F]">
+            <div class="h-full w-full ml-1 mt-6 bg-white rounded-full"></div>
+        </div>
+        <div class="absolute -z-2 -top-28 left-15 h-43 w-43 rounded-full bg-gradient-to-r from-[#9BFFCF] to-[#56929F]">
+            <div class="h-full w-full ml-5 mt-1 bg-white rounded-full"></div>
+        </div>
+        <div class="absolute -z-1 -top-41 left-44 h-57 w-57 rounded-full bg-gradient-to-r from-[#9BFFCF] to-[#56929F]">
+            <div class="h-50 w-50 mt-7 ml-1 rounded-full bg-white"></div>
+        </div>
+        <input type="text" placeholder="Type in Here" class="h-14 w-113 bg-white rounded-full focus:outline-none px-7 font-orbitron font-extrabold text-center text-[1.45rem] placeholder:text-[#AAAAAA] text-[#AAAAAA]">
+    </div>
+</template>
