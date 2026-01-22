ea61c294478e2ebd11c73e4f7fad16b684a81765
zzztzzzt <180073683+zzztzzzt@users.noreply.github.com>
2026-01-22 22:42:04 +0800

    [update] add simple RWD on InputMatchaTech

--- src/components/InputMatchaTech.vue
+++ src/components/InputMatchaTech.vue
@@ -4,11 +4,11 @@
 
 <template>
     <div>
-        <div class="flex justify-center items-center w-180 h-42 rounded-full bg-gradient-to-l from-white from-40% to-[#81FFAB]">
-            <div class="relative w-174 h-36 bg-white rounded-full">
-                <div class="w-130 h-30 absolute top-16 left-1/2 -translate-x-1/2 flex justify-center items-center rounded-full bg-gradient-to-l from-white from-40% to-[#58A770]">
-                    <div class="relative w-126 h-26 rounded-full bg-white">
-                        <div class="w-88 h-15 absolute -top-9 left-1/2 -translate-x-1/2 flex justify-center items-center rounded-full bg-[#AFAFAF]">
+        <div class="flex justify-center items-center max-w-screen w-180 h-42 rounded-full bg-gradient-to-l from-white from-40% to-[#81FFAB]">
+            <div class="relative flex items-center max-w-screen w-174 h-36 bg-white rounded-full">
+                <div class="max-w-screen w-130 h-30 lg:absolute lg:top-16 lg:left-1/2 lg:-translate-x-1/2 flex justify-center items-center rounded-full bg-gradient-to-l from-white from-40% to-[#58A770]">
+                    <div class="relative flex items-center max-w-screen w-126 h-26 rounded-full bg-white">
+                        <div class="w-88 h-15 lg:absolute lg:-top-9 lg:left-1/2 lg:-translate-x-1/2 flex justify-center items-center rounded-full bg-[#AFAFAF]">
                             <input type="text" class="w-86 h-13 px-5 text-lg bg-white rounded-full focus:outline-none">
                         </div>
                     </div>
